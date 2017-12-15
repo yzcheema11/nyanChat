5f0c74f70a6573a2e20d154d2e835155661bf0fd
zan <yzcheema11@gmail.com>
2017-12-15 17:06:12 -0500

    "Front End Complete. Logic is next"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
 import { ActiveChatComponent } from './active-chat/active-chat.component';
 import { MessageComponent } from './message/message.component';
 import { PostMessageComponent } from './post-message/post-message.component';
+import { HeadingImageComponent} from './heading-image/heading-image.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 @NgModule({
@@ -13,10 +16,15 @@ import { PostMessageComponent } from './post-message/post-message.component';
     AppComponent,
     ActiveChatComponent,
     MessageComponent,
-    PostMessageComponent
+    PostMessageComponent,
+    HeadingImageComponent,
+    SidebarComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
