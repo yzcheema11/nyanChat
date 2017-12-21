29b42ac6458f34bf4f366407d37844672d50b5f7
nehctuk <nehctuk@gmail.com>
2017-12-21 11:04:59 -0500

    Fixed unworking sidebar test

--- src/app/sidebar/sidebar.component.spec.ts
+++ src/app/sidebar/sidebar.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SidebarComponent } from './sidebar.component';
+import {FormsModule} from "@angular/forms";
+import {LoginComponent} from "../login/login.component";
 
 describe('SidebarComponent', () => {
   let component: SidebarComponent;
@@ -8,16 +10,14 @@ describe('SidebarComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SidebarComponent ]
-    })
-    .compileComponents();
-  }));
+      imports: [ FormsModule ],
+      declarations: [ SidebarComponent, LoginComponent ]
+    });
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(SidebarComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
